0ac1dbc3eca0dfe542eb0483f3fac1ce0a9bbc3a
SeoungHyuckWon <66349040+SeoungHyuckWon@users.noreply.github.com>
2022-11-28 18:11:21 +0900

    Update eveluate.py

--- model_training/MRC/eveluate.py
+++ model_training/MRC/eveluate.py
@@ -279,3 +279,27 @@ class exact_match():
                 exact_match += 1
 
         return (exact_match/total) * 100
+
+
+class levenshtein():
+    def __init__(self, s1, s2) -> None:
+        self.s1 = s1
+        self.s2 = s2
+
+    def levenshtein_score(self):
+        if len(s1) < len(s2):
+            return levenshtein(s2, s1)
+
+        if len(s2) == 0:
+            return len(s1)
+
+        previous_row = range(len(s2) + 1)
+        for i, c1 in enumerate(s1):
+            current_row = [i+1]
+            for j, c2 in enumerate(s2):
+                insertions = previous_row[j+1] + 1
+                deletions = current_row[j] + 1
+                substitutions = previous_row[j] + (c1 != c2)
+                current_row.append(min(insertions, deletions, substitutions))
+            previous_row = current_row
+        return previous_row[-1]
